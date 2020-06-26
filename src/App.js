2ebdf511af3d327eeca76331d8a92da8c1f60e4c
Hannah Davydova <hannah.davydova@gmail.com>
2020-06-26 14:20:14 +0100

    Only show results after search query

--- src/App.js
+++ src/App.js
@@ -11,23 +11,23 @@ class App extends Component {
     loading: false,
   };
 
-  componentDidMount() {
+  searchUsers = (text) => {
     this.setState({ loading: true });
     axios
       .get(
-        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
       )
       .then((res) => {
-        this.setState({ users: res.data, loading: false });
+        this.setState({ users: res.data.items, loading: false });
       });
-  }
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
